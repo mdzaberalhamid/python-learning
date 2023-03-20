5af6c5c5dc54fd125797999e910d92f9af546596
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-21 01:50:57 +0600

    Create 2_Python_Syntax.py

---
+++ 2_Python_Syntax.py
@@ -0,0 +1,16 @@
+# Python Print/Ouput
+print("Welcome!")
+
+# Python Indentation
+if 9 > 8:
+  print("Nine is greater than Eight")
+
+# Python Variables
+x = 5
+y = "Hi, I am Zaber."
+print(x)
+print(y)
+
+# Python Comment
+# Python comments start with #
+# This is also a Python comment
