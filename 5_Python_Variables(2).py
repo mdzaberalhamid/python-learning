335750cb53c375be3396ddbc5b6d25740f4dae98
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-22 01:21:59 +0600

    Create 5_Python_Variables(2).py

---
+++ 5_Python_Variables(2).py
@@ -0,0 +1,21 @@
+# Python Variables - Assign Multiple Values
+
+# Assigning many values to multiple variables
+a, b, c = 'Mango', 'Banana', 'Apple'
+print(a)
+print(b)
+print(c)
+# print(a, b, c)  # Try this also
+
+# Assigning one value to multiple variables
+d = e = f = 3
+print(d)
+print(e)
+print(f)
+
+# Unpacking values in a collection of list to var
+cars = ['Toyota', 'Nissan', 'Marcedes']
+c1, c2, c3 = cars
+print(c1)
+print(c2)
+print(c3)
