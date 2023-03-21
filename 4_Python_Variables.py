b133cbf9c0c774564ec02529c69e0e5a3d3f42ac
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-22 01:14:37 +0600

    Create 4_Python_Variables.py

---
+++ 4_Python_Variables.py
@@ -0,0 +1,29 @@
+# Creating variables in Python
+a = 1
+s = "Python"
+print(a, s)
+
+# Changing type of a variable
+b = 5
+b = "Hi, I am a String."
+print(b)
+
+# Casting
+c = int(3)  # c is int type variable
+d = str(3)  # d is string type variable
+e = float(3)  # e is float type variable
+
+print(c)
+print(d)
+print(e)
+
+# Get type of variables
+print(type(c))
+print(type(d))
+print(type(e))
+
+# Case sensitivity
+f = 8
+F = "Car"  # F will not overwrite f
+print(f)
+print(F)
