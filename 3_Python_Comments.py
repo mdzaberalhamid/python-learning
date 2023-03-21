5ffed1ff00d999ad1d5ca4565aa83cab65d837ae
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-22 00:47:25 +0600

    Create 3_Python_Comments.py

---
+++ 3_Python_Comments.py
@@ -0,0 +1,22 @@
+# Python single line comment
+print("Python single line comment starts with # (hash)")
+
+# Python multi-line
+# comment
+print("Python multi-line comment starts with # (hash) on every line.")
+
+# Preventing execution of a code
+# print("Hello World")
+print("Comment can be used to prevent execution of code.")
+
+# Comment on line end
+x = 111  # Comment at the end of a line.
+print(x)  # Will print the value of x
+
+# Multi-line string in triple quotes
+"""
+This is also a 
+comment which will 
+be ignored.
+"""
+print("Python multi-line strings in triple quotes.")
