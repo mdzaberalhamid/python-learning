fd9a215eb411f0a86dd5da0ce4a2e23038339f6d
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-03-23 00:36:22 +0600

    Create 6_Python_Output_Variables.py

---
+++ 6_Python_Output_Variables.py
@@ -0,0 +1,30 @@
+# Python - Output Variables
+
+# Output a variable
+a = "Hi"
+print(a)
+
+# Output multiple variables
+txt1 = 'I am'
+txt2 = 'learning'
+txt3 = 'Python!'
+print(txt1, txt2, txt3)
+
+# Output multiple variables using + operator
+txt4 = 'Good '
+txt5 = 'Luck!'
+print(txt4 + txt5)
+
+# Output a number
+num1 = 10
+print(num1)
+
+# Output a result
+num2, num3 = 5, 3
+print(num2 + num3)
+
+# Output different data types values
+x = 1
+y = 'Zaber'
+# print(x + y)  # Error
+print(x, y)
