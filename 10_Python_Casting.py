238edcc64cbfe79153b5fe6746a105e5935f8fcd
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-01 23:26:06 +0600

    Create 10_Python_Casting.py

---
+++ 10_Python_Casting.py
@@ -0,0 +1,32 @@
+# Python Casting
+
+# Specify int variable type
+x1 = int(1)
+x2 = int(3.2578)
+x3 = int("9")
+
+print(x1)  # 1
+print(x2)  # 3
+print(x3)  # 9
+print("")  # blank line
+
+# Specify float variable type
+y1 = float(1)
+y2 = float(3.76)
+y3 = float("5")
+y4 = float("9.999")
+
+print(y1)  # 1.0
+print(y2)  # 3.76
+print(y3)  # 5.0
+print(y4)  # 9.999
+print("")
+
+# Specify string variable type
+z1 = str("String 1")
+z2 = str(4)
+z3 = str(7.8901)
+
+print(z1)  # "String 1"
+print(z2)  # "4"
+print(z3)  # "7.8901"
