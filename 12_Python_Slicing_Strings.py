e7bbb1e75f8c1e47c7f05d27eb4ab096d8058abd
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-03 15:35:34 +0600

    Create 12_Python_Slicing_Strings.py

---
+++ 12_Python_Slicing_Strings.py
@@ -0,0 +1,17 @@
+# Python - Slicing Strings
+
+# Slicing
+txt1 = "Hello World!"
+print(txt1[2:5])  #llo
+
+# Slicing from the start
+txt2 = "string 2"
+print(txt2[:3])  # str
+
+# Slicing to the end
+txt3 = "string 3"
+print(txt3[5:])  # g 3
+
+# Negative indexing
+txt4 = "this is another string!"
+print(txt4[-7:-1])  # string
