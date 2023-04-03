49b3c056a31abbf5e51195c7f230f4ade67e93fa
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-03 16:13:45 +0600

    Create 13_Python_Modify_Strings.py

---
+++ 13_Python_Modify_Strings.py
@@ -0,0 +1,22 @@
+# Python - Modify Strings
+
+# Upper case
+str1 = "string lower case to upper case"
+print(str1.upper())
+
+# Lower case
+str2 = "STRING UPPER CASE TO LOWER CASE"
+print(str2.lower())
+
+# Removing whitespace
+str3 = "   Removing whitespaces   "
+print(str3.strip())
+
+# Replacing string
+str4 = "take"
+print(str4.replace("t", "m"))
+print(str4.replace("take", "lake"))
+
+# Spliting a string
+str5 = "Hello, World"
+print(str5.split(","))
