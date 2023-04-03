8e7215b30c28e2488445a37ff4c47f2137635ec5
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-04 04:17:52 +0600

    Create 14_Python_String_Concatenation.py

---
+++ 14_Python_String_Concatenation.py
@@ -0,0 +1,16 @@
+# Python - String Concatenation
+
+# String concatenation
+# example 1
+txt1 = "Python "
+txt2 = "Programming"
+txt3 = txt1 + txt2
+print(txt3)
+
+# example 2
+txt4 = "I am"
+txt5 = "Learning"
+txt6 = "Python!"
+
+str = txt4 + " " + txt5 + " " + txt6
+print(str)
