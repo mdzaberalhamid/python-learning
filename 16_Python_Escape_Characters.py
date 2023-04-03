4ce350ccf6255fefc30fa94027e2e5065582b12a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-04 04:25:18 +0600

    Create 16_Python_Escape_Characters.py

---
+++ 16_Python_Escape_Characters.py
@@ -0,0 +1,17 @@
+# Python - Escape Characters
+
+# use of \ (backslash) in string
+str_1 = "I am learning \"Python - Escape Characters\"!"
+print(str_1)
+
+# adding a \ (backslash)
+str_2 = "Adding one \\ (backslash)"
+print(str_2)
+
+# creating a new line in a string
+str_3 = "Hello \nWorld!"
+print(str_3)
+
+# tab in a string
+str_4 = "Hi,\t I am Zaber!"
+print(str_4)
