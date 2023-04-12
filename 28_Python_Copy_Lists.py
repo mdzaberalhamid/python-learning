e9a5cf7b76c04a219ff686610cbeadcddd78cebc
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-12 16:04:24 +0600

    Create 28_Python_Copy_Lists.py

---
+++ 28_Python_Copy_Lists.py
@@ -0,0 +1,14 @@
+# Python - Copy Lists
+print("Python - Copy Lists:")
+
+# Copy a list
+print("\nCopy a list:")
+print("Example 1:")
+list_1 = [1, 3, 9, 27]
+list_2 = list_1.copy()
+print(list_2)
+
+print("Example 2:")
+list_3 = ['red', 'green', 'blue']
+list_4 = list(list_3)
+print(list_4)
