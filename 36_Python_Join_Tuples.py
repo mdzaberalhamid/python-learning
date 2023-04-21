974b89f1f8d2e5919c7c014864aaf2cb7eeac559
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-21 17:27:40 +0600

    Create 36_Python_Join_Tuples.py

---
+++ 36_Python_Join_Tuples.py
@@ -0,0 +1,15 @@
+# Python - Join Tuples
+print("Python - Join Tuples:")
+
+# Join two tuples
+print("\nJoin two tuples:")
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Multiply tuple
+print("\nMultiply tuple:")
+tuple4 = (1, 2, 3)
+tuple5 = tuple4 * 2
+print(tuple5)
