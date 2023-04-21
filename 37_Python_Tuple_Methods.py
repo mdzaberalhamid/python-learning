f5c7413ee2ef18336baf0920b8fb2480e7e9d7fd
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-21 17:37:18 +0600

    Create 37_Python_Tuple_Methods.py

---
+++ 37_Python_Tuple_Methods.py
@@ -0,0 +1,18 @@
+# Python - Tuple Methods
+print("Python - Tuple Methods:")
+
+# Tuple count() method
+print("\nTuple count() method:")
+tuple1 = (1, 2, 3, 3, 5, 5, 4, 7, 6, 5, 5, 0, 9)
+
+print(tuple1.count(5))
+print(tuple1.count(3))
+print(tuple1.count(9))
+
+# Tuple index() method
+print("\nTuple index() method:")
+tuple2 = ('apple', 'banana', 'cherry', 'apple')
+
+print(tuple2.index('apple'))
+print(tuple2.index('banana'))
+print(tuple2.index('cherry'))
