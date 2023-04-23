8844150b42aa1a621b4ae7a9bcb373e3247f8a1a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-23 15:30:36 +0600

    Create 39_Python_Access_Set_Items.py

---
+++ 39_Python_Access_Set_Items.py
@@ -0,0 +1,17 @@
+# Python - Accessing Set Items
+print("Python - Accessing Set Items:")
+
+# Access items
+print("\nAccess items:")
+
+print("Example 1:")
+set1 = {'a', 'b', 'c'}
+for x in set1:
+  print(x)
+
+print("Example 2:")
+set2 = {2, 5, 1, 8, 9}
+# print(9 in set2)
+# or
+value1 = 9 in set2
+print(value1)
