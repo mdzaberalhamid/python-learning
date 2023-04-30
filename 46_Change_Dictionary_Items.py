a475215611763a7c1c406c010930f5223adf437a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-30 21:53:11 +0600

    Create 46_Change_Dictionary_Items.py

---
+++ 46_Change_Dictionary_Items.py
@@ -0,0 +1,14 @@
+# Python - Change Dictionary Items
+print("Python - Change Dictionary Items:")
+
+# Change values
+print("\nChange values:")
+d1 = {"str": "String 1", "int": 57, "bool": True}
+d1["int"] = 90
+print(d1)
+
+# Update dictionary
+print("\nUpdate dictionary:")
+d2 = {"name": "Zaber", "age": 29, "weight": 49}
+d2.update({"weight": 50})
+print(d2)
