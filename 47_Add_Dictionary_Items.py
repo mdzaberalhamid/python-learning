9b6c5abf9739128bce97584a06456db185743dba
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-04-30 22:00:17 +0600

    Create 47_Add_Dictionary_Items.py

---
+++ 47_Add_Dictionary_Items.py
@@ -0,0 +1,14 @@
+# Python - Add Dictionary Items
+print("Python - Add Dictionary Items:")
+
+# Adding items
+print("\nAdding items:")
+car1 = {"brand": "toyota", "model": "X", "year": 1980}
+print(car1)
+
+car1["color"] = "black"
+print(car1)
+
+# Using update() method
+car1.update({"price": 10000})
+print(car1)
