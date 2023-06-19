308594414bfb3decc3e3469edc97422a73b31f78
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-06-19 13:35:50 +0600

    Create 66_Python_Math.py

---
+++ 66_Python_Math.py
@@ -0,0 +1,35 @@
+# Python Math
+print("Python Math\n")
+
+# Built-in math functions
+print("Built-in math functions:\n")
+# max() and min()
+a = max(10, 5, 25)
+b = min(9, 3, 14)
+print('Max value:', a)  # 25
+print('Min value:', b)  # 3
+
+# abs() function
+c = abs(-3.65421)
+print('Absolute value:', c)
+
+# pow() function
+d = pow(2, 3)
+print('Power of 2^3:', d)  # 8
+
+# The math module
+print("\nThe math module:\n")
+import math
+# sqrt()
+e = math.sqrt(64)
+print('Square root of 64 is:', e)  # 8
+# ceil()
+f = math.ceil(2.6)
+print('Ceil value of 2.6 is:', f)  # 3
+# floor()
+g = math.floor(2.6)
+print('Floor value of 2.6 is:', g)  # 2
+
+# math.pi constant
+h = math.pi
+print('Value of PI is:', h)  # 3.14159...
