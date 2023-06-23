83cd41e1626a092b05a70f923247d591d990d838
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-06-23 15:07:34 +0600

    Create 69_Python_PIP.py

---
+++ 69_Python_PIP.py
@@ -0,0 +1,12 @@
+# Python PIP
+print("Python PIP")
+
+# Using a Package
+print("\nUsing a Package:")
+
+import camelcase
+
+x = camelcase.CamelCase()
+txt = "hi welcome!"
+# print(txt)
+print(x.hump(txt))
