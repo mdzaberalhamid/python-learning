479c4a5abba82b696341b3c35d84b34ed1c842b5
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-07-03 01:17:21 +0600

    Add files via upload

---
+++ 73_Python_File_Open/main.py
@@ -0,0 +1,9 @@
+# Python File Open
+print("Python File Open\n")
+
+f1 = open("demofile.txt", "r")
+print(f1)  # object
+print(f1.read())  # show contents
+
+# f2 = open("demofile2.txt", "r")
+# error (demofile2.txt file not exist)
