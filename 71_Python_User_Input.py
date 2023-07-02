572448ea27ff18a9edd8b081de1f1cabf27d2d5c
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-07-02 15:25:58 +0600

    Create 71_Python_User_Input.py

---
+++ 71_Python_User_Input.py
@@ -0,0 +1,12 @@
+# Python User Input
+print("Python User Input\n")
+
+print("User Input:")
+
+print("\nExample:")
+username = input("Enter username: ")
+print("Welcome " + username)
+
+print("\nExample:")
+usernumber = input("Now, enter a number: ")
+print("User number is: " + usernumber)
