b2c8589b040192a5f5f5c58a9b02583bf704d620
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-03 21:55:52 +0600

    Update 80_Python_Matplotlib_Markers.py

--- 80_Python_Matplotlib_Markers.py
+++ 80_Python_Matplotlib_Markers.py
@@ -5,5 +5,21 @@ import numpy as np
 
 ypoints = np.array([3,8,1,10])
 
-plt.plot(ypoints, marker = 's')
+#Markers
+
+#plt.plot(ypoints, marker = 's')
+#plt.show()
+
+# Format Strings
+
+#plt.plot(ypoints, 'o:r')
+#plt.plot(ypoints, 'o-g')
+#plt.plot(ypoints, 'o--b')
+#plt.show()
+
+#Marker Size, Color, Face Color
+
+#plt.plot(ypoints, marker = 'o', ms = 10)
+#plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r')
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'g', mfc = 'r')
 plt.show()
