d15df0e5f5c95dc3f9ef1578d0340bf450413e28
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-09 21:35:02 +0600

    Update 82_Matplotlib_Labels.py

--- 82_Matplotlib_Labels.py
+++ 82_Matplotlib_Labels.py
@@ -7,10 +7,15 @@ import numpy as np
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.plot(x,y)
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
 
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-plt.title("Sports Watch Data")
+#plt.title("Sports Watch Data", fontdict = font1)
+plt.xlabel("Average Pulse", fontdict = font2)
+plt.ylabel("Calorie Burnage", fontdict = font2)
 
+#Positioning the title
+plt.title("Sports Watch Data", fontdict = font1, loc = "left")
+
+plt.plot(x, y)
 plt.show()
