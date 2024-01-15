26ec58a02dd67883e7dc5aeddb8bd5e5d41039b6
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-15 22:49:38 +0600

    Create 88_Matplotlib_Scatter_Part_3.py

---
+++ 88_Matplotlib_Scatter_Part_3.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Matplotlib Scatter - Part 3
+
+#Combine Color Size and Alpha
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = 10 * np.random.randint(100, size=(100))
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.5, cmap='nipy_spectral')
+
+plt.colorbar()
+
+plt.show()
