73f0195c7c7d8cbf59748b286e8cd8945c87de16
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2024-01-17 22:44:22 +0600

    Update 89_Matplotlib_Bars.py

--- 89_Matplotlib_Bars.py
+++ 89_Matplotlib_Bars.py
@@ -8,9 +8,9 @@ x = np.array(["A", "B", "C", "D"])
 y = np.array([3, 8, 1, 10])
 
 plt.bar(x,y)
-#plt.barh(x,y)                        #Horizontal Bars
-#plt.bar(x, y, color='red')       #Bars Color
-#plt.bar(x, y, width = 0.1)     #Bars Width
+#plt.barh(x,y)                 #Horizontal Bars
+#plt.bar(x, y, color='red')    #Bars Color
+#plt.bar(x, y, width = 0.1)    #Bars Width
 #plt.barh(x, y, height = 0.1)  #Bars Width in Horizontal
 
 plt.show()
